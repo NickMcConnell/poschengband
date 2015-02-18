9a9e056dd9744e244f9d9a6271861e97bf56f226
poschengband <ckousky@gmail.com>
2015-02-18 13:46:44 -0600

    Code Cleanup: Keep GCC happy on Linux

--- src/util.c
+++ src/util.c
@@ -2533,7 +2533,7 @@ void message_add(cptr str)
         }
 
         /* Limit the multiplier to 1000 */
-        if (buf && streq(buf, str) && (j < 1000))
+        if (streq(buf, str) && (j < 1000))
         {
             j++;
 
